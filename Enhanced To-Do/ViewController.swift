836e94f16dfe1c43e310c31c8a7c6db0d255a76c
dogukan-k <karayilanoglu35@hotmail.com>
2019-11-18 17:04:28 -0500

    ViewController Delegate and DataSource

---
+++ Enhanced To-Do/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Enhanced To-Do
+//
+//  Created by DKU on 18.11.2019.
+//  Copyright © 2019 dku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
+ 
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         <#code#>
+     }
+     
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         <#code#>
+     }
+     
+
+}
+
