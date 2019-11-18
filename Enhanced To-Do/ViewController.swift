8989c8d7f61b7806ae20921e53fe406e2c907357
dogukan-k <karayilanoglu35@hotmail.com>
2019-11-18 18:18:11 -0500

    Extension Custom Colors added

--- Enhanced To-Do/ViewController.swift
+++ Enhanced To-Do/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
  
+    //xcdatamodeld uzantili yerde tanimladigimiz Item listi olusturuyoruz.
+    var itemsList = [Items]() ;
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,13 +20,48 @@ class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSo
 
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-         <#code#>
+          return itemsList.count;
      }
      
      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-         <#code#>
+           let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath);
+               let item = itemsList[indexPath.row];
+               
+        
+               cell.textLabel?.text = item.name ;
+               cell.accessoryType = item.completed ? .checkmark : .none
+               //Background color of cell is up to type of item
+               cell.backgroundColor = UIColor.init(named: item.strColor!);
+         
+               
+               return cell ;
      }
      
 
+}
+
+    //Custom Colors
+    extension UIColor {
+    
+    convenience init(hexString: String) {
+        // Trim leading '#' if needed
+        var cleanedHexString = hexString
+        if hexString.hasPrefix("#") {
+            
+            cleanedHexString = String(hexString.dropFirst())
+        }
+        
+        // String -> UInt32
+        var rgbValue: UInt32 = 0
+        Scanner(string: cleanedHexString).scanHexInt32(&rgbValue)
+        
+        // UInt32 -> R,G,B
+        let red = CGFloat((rgbValue >> 16) & 0xff) / 255.0
+        let green = CGFloat((rgbValue >> 08) & 0xff) / 255.0
+        let blue = CGFloat((rgbValue >> 00) & 0xff) / 255.0
+        
+        self.init(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+    
 }
 
