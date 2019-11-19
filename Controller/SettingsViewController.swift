922bf8cb1a23998a7323250d4254e4423cc543e3
dogukan-k <karayilanoglu35@hotmail.com>
2019-11-19 03:15:04 -0500

    settingsviewcontroller added and items are binded

---
+++ Controller/SettingsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SettingsViewController.swift
+//  Enhanced To-Do
+//
+//  Created by DKU on 19.11.2019.
+//  Copyright © 2019 dku. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func shakeGestureSwitch(_ sender: UISwitch) {
+        
+        
+    }
+    
+    
+    @IBAction func todoAlertSwitch(_ sender: UISwitch) {
+        
+        
+    }
+    
+    
+
+}
